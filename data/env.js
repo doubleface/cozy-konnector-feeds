35fb62f155921e7e8f3d2f499e1b00086a416373
doubleface <christophe@thiriot.xyz>
2017-04-02 00:17:19 +0200

    Added a proposition of fields list in manifest and env

--- data/env.js
+++ data/env.js
@@ -3,6 +3,7 @@ const path = require('path')
 module.exports = {
   COZY_CREDENTIALS: fs.readFileSync(path.join(__dirname, 'token.json')),
   COZY_URL: 'http://cozy.local:8080',
+  COZY_FIELDS: '{"nb": 5}',
   NODE_ENV: 'development',
   DEBUG: '*'
 }
