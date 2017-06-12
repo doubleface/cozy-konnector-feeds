5dde9f04736d4f024864f3d2e1c83704a6250090
doubleface <christophe@thiriot.xyz>
2017-06-12 23:41:44 +0200

    fix: updated from cozy-konnector-template

--- data/env.js
+++ data/env.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 const path = require('path')
 const tokenPath = path.join(__dirname, 'token.json')
+
 module.exports = {
-  COZY_CREDENTIALS: fs.existsSync(tokenPath) ? fs.readFileSync(tokenPath) : 'NO TOKEN',
-  COZY_URL: 'http://cozy.tools:8080',
-  COZY_FIELDS: '{"nb": 5}',
-  DEBUG: '*'
+  NODE_ENV: 'development',
+  COZY_CREDENTIALS: fs.existsSync(tokenPath) ? fs.readFileSync(tokenPath, 'utf-8') : 'NO TOKEN',
+  COZY_URL: 'http://cozy.tools:8080'
 }
